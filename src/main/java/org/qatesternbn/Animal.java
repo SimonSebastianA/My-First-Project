513d6e9772b0b4bfe3a631a23193abcb76bdb79e
SimonSebastianA <simonsebastian01@yahoo.com>
2023-11-01 16:20:44 +0200

    Create Animal.java!

---
+++ src/main/java/org/qatesternbn/Animal.java
@@ -0,0 +1,13 @@
+package org.qatesternbn;
+
+public class Animal {
+
+    String name;
+    double age;
+    double healthLevel;
+    double hungerLevel;
+    double happinessLevel;
+    String favoriteActivityName;
+    String favoriteFoodName;
+
+}
